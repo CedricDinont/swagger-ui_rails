0eefa0ab131ae02fd10affc9f512cd60886813ad
Alexander <aheaven87@gmail.com>
2016-01-17 19:15:16 +0200

    Upgrade swagger to 2.1.4

--- lib/swagger-ui_rails/engine.rb
+++ lib/swagger-ui_rails/engine.rb
@@ -3,10 +3,10 @@ module Swagger
 
     initializer :assets do |config|
       Rails.application.config.assets.precompile += %w{ swagger-ui.js }
-      Rails.application.config.assets.precompile += %w{ swagger-oauth.js }
       Rails.application.config.assets.precompile += %w{ swagger-ui.css }
       Rails.application.config.assets.precompile += %w{ swagger-ui/reset.css }
       Rails.application.config.assets.precompile += %w{ swagger-ui/screen.css }
+      Rails.application.config.assets.precompile += %w{ swagger-ui/print.css }
       Rails.application.config.assets.paths << root.join("app", "assets")
     end
 
